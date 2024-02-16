12984966aac02eb3ced895dc724e6b1131287631
Eric Gibson <144487367+gibso632@users.noreply.github.com>
2024-02-15 21:24:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,40 +12,42 @@ import psycopg2
 app = Flask(__name__)
 
 # Create variables with sign-in details
-database = 'gis5572'
+database = 'lab0'
 user = 'postgres'
 password = 'ATLAisagreatshow2020'
 host = '35.184.2.25'
 port = '5432'
 
 # Create a route to return polygon in GeoJSON format
-@app.route('/geojson_polygon')
+@app.route('/')
 def get_gj():
     # Connect to DB
-    conn = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
+    conn = psycopg2.connect(dbname=database, user=user, password=password, host=host, port=port)
     # Create cursor
     cursor = conn.cursor()
 
     # Creates GeoJSON string based on polygon stored in PostgreSQL DB
     sql_query = """
-    SELECT
-        json_build_object(
-            "type","FeatureCollection",
-            "features",json_agg(
+    SELECT 
+            json_build_object(
+                'type', 'FeatureCollection',
+                'features', json_agg(
+                    json_build_object(
+                        'type', 'Feature',
+                        'geometry', ST_AsGeoJSON(ST_SetSRID(geom, 4326))::json,
+                        'properties', json_build_object()
+                    )
+                ),
+                'crs', 
                 json_build_object(
-                    "type","Feature",
-                    "geometry",ST_AsGeoJSON(ST_SetSRID(shape, 4326))::json,
-                    "properties",json_build_object()
+                    'type', 'name',
+                    'properties', 
+                    json_build_object(
+                        'name', 'epsg:4326'
+                    )
                 )
-            ),
-            crs,json_build_object(
-                "type","name",
-                "properties",json_build_object(
-                    "name","epsg:4326"
-                )
-            )
-        ) AS geojson
-    FROM lab1
+            ) AS geojson
+        FROM lab0.public.lab1
     """
 
     # Gather polygon geojson from above query
@@ -62,6 +64,5 @@ if __name__ == "__main__":
     app.run(
         debug = False,
         host = '0.0.0.0',
-        port = int(os.environ.get("PORT", 8080))
+        port = int(os.environ.get("PORT", 5500))
     )
-

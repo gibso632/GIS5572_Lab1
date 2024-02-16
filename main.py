d95bcfc8b82ebcb1bab2568dd3c438bc28c25d1c
Eric Gibson <144487367+gibso632@users.noreply.github.com>
2024-02-15 22:33:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -64,5 +64,5 @@ if __name__ == "__main__":
     app.run(
         debug = False,
         host = '0.0.0.0',
-        port = int(os.environ.get("PORT", 5500))
+        port = int(os.environ.get("PORT", 5100))
     )
